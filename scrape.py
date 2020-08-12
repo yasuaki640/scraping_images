692aa9f2ebe24bdb28de6528a9b0b960792b583a
yasuaki640 <yasuaki640@gmail.com>
2020-08-12 17:17:47 +0900

    add config of user agent

--- scrape.py
+++ scrape.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-hiroshi_res = requests.get('http://abehiroshi.la.coocan.jp/')
-hiroshi_res.encoding = 'shift_jis'
+ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
+url = 'http://abehiroshi.la.coocan.jp/'
 
+hiroshi_res = requests.get(url, headers={'User-Agent': ua})
+hiroshi_res.encoding = 'shift_jis'
 hiroshi_html = BeautifulSoup(hiroshi_res.text, 'html.parser')
 
-title = str(hiroshi_html.title.string)
-print(title)
+print(hiroshi_html)
