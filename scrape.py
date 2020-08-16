35a5f5f82b7b8541992bdb97f1d52bc640699621
yasuaki640 <yasuaki640@gmail.com>
2020-08-16 17:02:59 +0900

    get his portrait src

--- scrape.py
+++ scrape.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-hiroshi_res = requests.get('http://abehiroshi.la.coocan.jp/')
+hiroshi_res = requests.get('http://abehiroshi.la.coocan.jp/top.htm')
 hiroshi_res.encoding = 'shift_jis'
 
 hiroshi_html = BeautifulSoup(hiroshi_res.text, 'html.parser')
 
-title = str(hiroshi_html.title.string)
-print(hiroshi_html)
+print(hiroshi_html.find('img')['src'])
+
+
