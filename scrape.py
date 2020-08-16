cc8e57e557182c9c1159f8faeb4a2abf116bdd26
yasuaki640 <yasuaki640@gmail.com>
2020-08-16 16:25:17 +0900

    Revert "rewrite by selenium"
    This reverts commit 1806eb49224e49e0d6f497018a58e958f5eb0088.

--- scrape.py
+++ scrape.py
@@ -1,11 +1,11 @@
-from selenium import webdriver
-import time
+import requests
+from bs4 import BeautifulSoup
 
-driver = webdriver.Chrome('chromedriver.exe')
-driver.get('http://abehiroshi.la.coocan.jp/')
-time.sleep(5)
-driver.close()
-# TODO:何も条件判定せずただ待つだけの
-#  よろしくない方法なのでseleniumで書き直す
-driver.quit()
+ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
+url = 'http://abehiroshi.la.coocan.jp/'
 
+hiroshi_res = requests.get(url, headers={'User-Agent': ua})
+hiroshi_res.encoding = 'shift_jis'
+hiroshi_html = BeautifulSoup(hiroshi_res.text, 'html.parser')
+
+print(hiroshi_html)
