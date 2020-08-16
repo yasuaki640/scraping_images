20e69a53fd2a13a292ccaac2f7829f5e2927fa87
yasuaki640 <yasuaki640@gmail.com>
2020-08-16 16:49:18 +0900

    modosu

--- scrape.py
+++ scrape.py
@@ -1,17 +1,10 @@
 import requests
-import urllib.request
 from bs4 import BeautifulSoup
 
-ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 ' \
-     'Safari/537.36 '
-url = 'http://abehiroshi.la.coocan.jp/'
-data = None
-headers = {'User-Agent': ua}
-
-hiroshi_req = urllib.request.Request(url, data, headers)
-
-hiroshi_res = urllib.request.urlopen(hiroshi_req)
+hiroshi_res = requests.get('http://abehiroshi.la.coocan.jp/')
 hiroshi_res.encoding = 'shift_jis'
-hiroshi_html = BeautifulSoup(hiroshi_res.read(), 'html.parser')
 
+hiroshi_html = BeautifulSoup(hiroshi_res.text, 'html.parser')
+
+title = str(hiroshi_html.title.string)
 print(hiroshi_html)
