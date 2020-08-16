1c2e06fa8ab4f79ca2605b0d86e525fce45d54fc
yasuaki640 <yasuaki640@gmail.com>
2020-08-16 16:41:15 +0900

    change http access lib and set ua

--- scrape.py
+++ scrape.py
@@ -1,11 +1,17 @@
 import requests
+import urllib.request
 from bs4 import BeautifulSoup
 
-ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
+ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 ' \
+     'Safari/537.36 '
 url = 'http://abehiroshi.la.coocan.jp/'
+data = None
+headers = {'User-Agent': ua}
 
-hiroshi_res = requests.get(url, headers={'User-Agent': ua})
+hiroshi_req = urllib.request.Request(url, data, headers)
+
+hiroshi_res = urllib.request.urlopen(hiroshi_req)
 hiroshi_res.encoding = 'shift_jis'
-hiroshi_html = BeautifulSoup(hiroshi_res.text, 'html.parser')
+hiroshi_html = BeautifulSoup(hiroshi_res.read(), 'html.parser')
 
 print(hiroshi_html)
