18a70e834036f0b4e16980d0c58916d2c6ca675a
yasuaki640 <yasuaki640@gmail.com>
2020-08-20 21:33:32 +0900

    download image by kuso code

--- scrape.py
+++ scrape.py
@@ -10,13 +10,22 @@ def get_html(url, encode):
     return html
 
 
+def download_img(url, path):
+    try:
+        img = requests.get(url)
+        open(path, 'wb').write(img.content)
+    except ValueError:
+        print('Value error occured')
+
+
 url = 'http://abehiroshi.la.coocan.jp/top.htm'
 html = get_html(url, 'shift-jis')
-img_path = html.find('img')['src']
+img_url = html.find('img')['src']
 
-img = requests.get('http://abehiroshi.la.coocan.jp/' + img_path)
+download_img('http://abehiroshi.la.coocan.jp/abe-top-20190328-2.jpg',
+             'C:/Users/yasua/PycharmProjects/scraping_images/img/abe_hiroshi.jpg')
 
-print(img_path)
+print(img_url)
 
 # やること
 # 画像をrequestとか使ってローカルに落とす
