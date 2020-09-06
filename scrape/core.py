82d0f80de6f7bdd31c972de97494ddf90dc72f4e
yasuaki640 <yasuaki640@gmail.com>
2020-09-07 06:14:31 +0900

    def main func and refactoro

--- scrape/core.py
+++ scrape/core.py
@@ -5,6 +5,28 @@ import re
 import cv2
 
 
+def main():
+    base_url = 'https://www.instagram.com/explore/tags/'
+    keyword = input('Enter keyword of images -->')
+
+    soup = get_soup(base_url + keyword, 'utf-8')
+    js_scripts = soup.select('script[type="text/javascript"]')
+    jpg_jsons = get_jpg_jsons(js_scripts)
+
+    with open('../url_output/img_paths.json', 'w') as js_file:
+        jpg_json = jpg_jsons[0].strip('<script type="text/javascript">window._sharedData = ').strip(';</script>')
+        js_file.write(jpg_json)
+
+    with open('../url_output/img_paths.json', 'r') as json_file:
+        img_path_json = json.load(json_file)
+        with open('../url_output/img_paths_formatted.json', 'w') as formatted_json_file:
+            json.dump(img_path_json, formatted_json_file, ensure_ascii=True, indent=4, separators=(',', ': '))
+
+    img_urls = extract_text_in_file('../url_output/img_paths_formatted.json', '"display_url": "', '",')
+    for url in img_urls:
+        print(url)
+
+
 def get_soup(url, encode):
     ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
     res = requests.get(url, headers={'User-Agent': ua})
@@ -41,23 +63,4 @@ def extract_text_in_file(filepath, pattern_prev, pattern_next):
 
 
 if __name__ == '__main__':
-    base_url = 'https://www.instagram.com/explore/tags/'
-    keyword = input('Enter keyword of images -->')
-
-    soup = get_soup(base_url + keyword, 'utf-8')
-    js_scripts = soup.select('script[type="text/javascript"]')
-    jpg_jsons = get_jpg_jsons(js_scripts)
-
-    with open('../url_output/img_paths.json', 'w') as js_file:
-        jpg_json = jpg_jsons[0].strip('<script type="text/javascript">window._sharedData = ').strip(';</script>')
-        js_file.write(jpg_json)
-
-    with open('../url_output/img_paths.json', 'r') as json_file:
-        img_path_json = json.load(json_file)
-
-    with open('../url_output/img_paths_formatted.json', 'w') as f:
-        json.dump(img_path_json, f, ensure_ascii=True, indent=4, separators=(',', ': '))
-
-    img_urls = extract_text_in_file('../url_output/img_paths_formatted.json', '"display_url": "', '",')
-    for url in img_urls:
-        print(url)
+    main()
