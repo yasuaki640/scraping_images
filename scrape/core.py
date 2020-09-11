3ccab5489814f32bffa31a991116ccc7be7c2e1a
yasuaki640 <yasuaki640@gmail.com>
2020-09-11 21:53:05 +0900

    refactor

--- scrape/core.py
+++ scrape/core.py
@@ -15,19 +15,19 @@ def main():
 
     BASE_URL = 'https://www.instagram.com/explore/tags/'
     soup = get_soup(BASE_URL + keyword, 'utf-8')
-    js_scripts = soup.select('script[type="text/javascript"]')
-    jpg_jsons = get_jpg_jsons(js_scripts)
+    script_tags = soup.select('script[type="text/javascript"]')
+    jpg_script_tags = get_jpg_scrip_tags(script_tags)
 
-    if len(jpg_jsons) <= 0:
+    if len(jpg_script_tags) <= 0:
         print('No image found.')
         return
 
     JSON_FILE_PATH = '../url_output/img_paths.json'
     FORMATTED_JSON_FILE_PATH = '../url_output/img_paths_formatted.json'
 
-    format_json_file(jpg_jsons, JSON_FILE_PATH, FORMATTED_JSON_FILE_PATH)
+    format_json_file(jpg_script_tags, JSON_FILE_PATH, FORMATTED_JSON_FILE_PATH)
 
-    img_urls = extract_text_in_file(FORMATTED_JSON_FILE_PATH, '"display_url": "', '",')
+    img_urls = extract_str_in_file(FORMATTED_JSON_FILE_PATH, '"display_url": "', '",')
     download_imgs(keyword, img_urls)
     print(str(len(img_urls)) + ' images downloaded')
 
@@ -40,32 +40,32 @@ def get_soup(url, encode):
     return soup
 
 
-def get_jpg_jsons(js_scripts):
-    jpg_jsons = []
+def get_jpg_scrip_tags(script_tags):
+    jpg_script_tags = []
 
-    for script in js_scripts:
-        if '.jpg' in str(script):
-            jpg_jsons.append(str(script))
-    return jpg_jsons
+    for script_tag in script_tags:
+        if '.jpg' in str(script_tag):
+            jpg_script_tags.append(str(script_tag))
+    return jpg_script_tags
 
 
-def extract_text_in_file(filepath, pattern_prev, pattern_next):
-    extracted_text_array = []
+def extract_str_in_file(file_path, pattern_prev, pattern_next):
+    extracted_strings = []
     pattern = pattern_prev + '(.*)' + pattern_next
 
-    with open(filepath) as f:
+    with open(file_path) as f:
         lines = f.readlines()
         for line in lines:
-            tmp_extracted_text_array = re.findall(pattern, line)
-            extracted_text_array.extend(tmp_extracted_text_array)
+            extracted_str = re.findall(pattern, line)
+            extracted_strings.extend(extracted_str)
 
-    return extracted_text_array
+    return extracted_strings
 
 
-def format_json_file(jpg_jsons, json_file_path, formatted_json_file_path):
-    with open(json_file_path, 'w') as js_file:
-        jpg_json = jpg_jsons[0].strip('<script type="text/javascript">window._sharedData = ').strip(';</script>')
-        js_file.write(jpg_json)
+def format_json_file(jpg_script_tag, json_file_path, formatted_json_file_path):
+    with open(json_file_path, 'w') as json_file:
+        img_path_json = jpg_script_tag[0].strip('<script type="text/javascript">window._sharedData = ').strip(';</script>')
+        json_file.write(img_path_json)
 
     with open(json_file_path, 'r') as json_file:
         img_path_json = json.load(json_file)
