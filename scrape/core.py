a29d96dd76da61b2b42cf61275e849c8660ff662
yasuaki640 <yasuaki640@gmail.com>
2020-10-11 21:20:34 +0900

    write todo

--- scrape/core.py
+++ scrape/core.py
@@ -64,6 +64,7 @@ def extract_str_in_file(file_path, pattern_prev, pattern_next):
 
 def format_json_file(jpg_script_tag, json_file_path, formatted_json_file_path):
     with open(json_file_path, 'w') as json_file:
+        # TODO: Treat text in tag as JS
         img_path_json = jpg_script_tag[0].strip('<script type="text/javascript">window._sharedData = ').strip(
             ';</script>')
         json_file.write(img_path_json)
