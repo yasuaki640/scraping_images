6acea057088ce0bee4b8685c892afc8ca2e5c002
yasuaki640 <yasuaki640@gmail.com>
2020-10-13 22:19:48 +0900

    call test target func

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,11 +2,13 @@ import unittest
 
 BASE_URL = 'https://www.instagram.com/explore/tags/'
 KEYWORD = '今田美桜'
+ENCODE = 'utf-8'
 
 
 class GetImagesTest(unittest.TestCase):
     def test_get_soup(self):
         from scrape.core import get_soup
+        soup = get_soup(BASE_URL + '/' + KEYWORD, ENCODE)
         self.assertEqual(True, False)
 
 
