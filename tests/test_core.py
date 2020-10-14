db3f1286030773334f471c6a76ceddfc32d79768
yasuaki640 <yasuaki640@gmail.com>
2020-10-14 22:15:07 +0900

    def func

--- tests/test_core.py
+++ tests/test_core.py
@@ -11,6 +11,7 @@ class GetImagesTest(unittest.TestCase):
         soup = get_soup(BASE_URL + '/' + KEYWORD, ENCODE)
         self.assertEqual(True, soup.prettify().__contains__('<!DOCTYPE html>'))
 
+    def test_get_jpg_scrip_tags(self):
 
 if __name__ == '__main__':
     unittest.main()
