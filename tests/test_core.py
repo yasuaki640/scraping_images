534c3b5cb9345ab1b42a1309fd40e0bdbc3b2188
yasuaki640 <yasuaki640@gmail.com>
2020-10-17 19:55:10 +0900

    import test data

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,5 @@
 import unittest
+import tests.data.data as data
 
 BASE_URL = 'https://www.instagram.com/explore/tags/'
 KEYWORD = '今田美桜'
@@ -12,9 +13,8 @@ class GetImagesTest(unittest.TestCase):
         self.assertEqual(True, soup.prettify().__contains__('<!DOCTYPE html>'))
 
     def test_get_jpg_scrip_tags(self):
-        from scrape.core import get_soup
-        soup = get_soup(BASE_URL + '/' + KEYWORD, ENCODE)
         from scrape.core import get_jpg_scrip_tags
+        jpg_script_tags = get_jpg_scrip_tags(data.script_tags)
 
 
 if __name__ == '__main__':
