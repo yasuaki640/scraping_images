0f9e314abd0546e1321d2ef932ebcba260866cee
yasuaki640 <yasuaki640@gmail.com>
2020-10-17 20:28:22 +0900

    alter call func of judge result

--- tests/test_core.py
+++ tests/test_core.py
@@ -10,7 +10,7 @@ class GetImagesTest(unittest.TestCase):
     def test_get_soup(self):
         from scrape.core import get_soup
         soup = get_soup(BASE_URL + '/' + KEYWORD, ENCODE)
-        self.assertEqual(True, soup.prettify().__contains__('<!DOCTYPE html>'))
+        self.assertTrue(soup.prettify().__contains__('<!DOCTYPE html>'))
 
     def test_get_jpg_scrip_tags(self):
         from scrape.core import get_jpg_scrip_tags
