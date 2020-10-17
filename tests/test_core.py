2176690954c5d7cc09735d1d9ab0e64be0528312
yasuaki640 <yasuaki640@gmail.com>
2020-10-18 06:55:50 +0900

    change var name retain script tags

--- tests/test_core.py
+++ tests/test_core.py
@@ -15,10 +15,10 @@ class GetImagesTest(unittest.TestCase):
 
     def test_get_jpg_scrip_tags(self):
         from scrape.core import get_jpg_scrip_tags
-        jpg_script_tags = get_jpg_scrip_tags(data.script_tags)
+        script_tags = get_jpg_scrip_tags(data.script_tags)
 
         contain_jpg = True
-        for tag in jpg_script_tags:
+        for tag in script_tags:
             if not tag.__contains__('.jpg'):
                 contain_jpg = False
 
