9c8a208321a69cf575713df91f5c39c973fb870d
yasuaki640 <yasuaki640@gmail.com>
2020-10-22 22:20:25 +0900

    create test code

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,7 @@
+import json
 import pathlib
+import sys
+import traceback
 import unittest
 from tempfile import TemporaryDirectory
 import tests.data.data as data
@@ -37,6 +40,20 @@ class GetImagesTest(unittest.TestCase):
     def test_get_json_contains_url(self):
         from scrape.core import get_json_contains_url
         str = get_json_contains_url(data.jpg_script_tags)
+
+        result = True
+
+        # Check if the string is in json format
+        try:
+            json.loads(str)
+        except json.JSONDecodeError as e:
+            traceback.print_exc(file=sys.stdout)
+            result = False
+
+        if not str.__contains__('.jpg'):
+            result = False
+
+        self.assertTrue(result)
 
     def test_download_imgs(self):
         from scrape.core import download_imgs
