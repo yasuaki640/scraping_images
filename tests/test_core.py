dcf26dc58cc49af3544fc561492cf1ff211a0079
yasuaki640 <yasuaki640@gmail.com>
2020-10-24 16:35:52 +0900

    devide one test as two

--- tests/test_core.py
+++ tests/test_core.py
@@ -35,23 +35,29 @@ class GetImagesTest(unittest.TestCase):
 
         self.assertTrue(contain_jpg)
 
-    def test_get_json_contains_url(self):
+    def test_get_json_contains_url_given_json_as_param(self):
         from scrape.core import get_json_contains_url
-        str = get_json_contains_url(data.jpg_script_tags)
+        return_str = get_json_contains_url(data.jpg_script_tags)
 
-        result = True
-
+        is_json = False
         # Check if the string is in json format
         try:
-            json.loads(str)
+            json.loads(return_str)
+            is_json = True
         except json.JSONDecodeError as e:
             traceback.print_exc(file=sys.stdout)
-            result = False
 
-        if not str.__contains__('.jpg'):
-            result = False
+        self.assertEqual(is_json, True)
 
-        self.assertTrue(result)
+    def test_get_json_contains_url_given_str_contains_url_as_param(self):
+        from scrape.core import get_json_contains_url
+        return_str = get_json_contains_url(data.jpg_script_tags)
+
+        contains_jpg = False
+        if return_str.__contains__('.jpg'):
+            contains_jpg = True
+
+        self.assertTrue(contains_jpg, True)
 
     def test_download_imgs(self):
         from scrape.core import download_imgs
